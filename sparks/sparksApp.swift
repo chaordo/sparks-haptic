9f4b48604fb987e869f761a2f67418486e8204d6
quintus <john@chaordo.com>
2023-08-10 10:51:39 -0600

    Initial Commit

---
+++ sparks/sparksApp.swift
@@ -0,0 +1,17 @@
+//
+//  sparksApp.swift
+//  sparks
+//
+//  Created by John on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct sparksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
